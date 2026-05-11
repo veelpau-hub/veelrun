f645157632222e32a333e5739d1947b075140fd7
PauVid <pauvidalillanos@gmail.com>
2026-05-11 20:25:26 +0200

    feat: cache-control max-age=60 on static files

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+  setHeaders: (res) => res.set('Cache-Control', 'public, max-age=60'),
+}));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
